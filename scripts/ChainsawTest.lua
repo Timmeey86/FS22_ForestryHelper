b0aa8543c0098c817279b6fb2483d6d3651cba32
Timmeey86 <Timmeey86@hotmail.com>
2024-01-19 20:29:21 +0100

    detect radius at any distance left/right of the ring selector

--- scripts/ChainsawTest.lua
+++ scripts/ChainsawTest.lua
@@ -4,6 +4,8 @@ local ChainsawTest_mt = Class(ChainsawTest)
 function ChainsawTest.new()
     local self = setmetatable({}, ChainsawTest_mt)
     self.woodProbeNode = createTransformGroup("treeValueInfo_woodProbeNode")
+    self.debugNode = createTransformGroup("treeValueInfo_debugNode")
+    link(getRootNode(), self.debugNode)
     return self
 end
 
@@ -16,16 +18,61 @@ end
 end]]
 
 function ChainsawTest:afterChainsawPostLoad(chainsaw)
+    print("afterChainsawPostLoad")
     link(chainsaw.chainsawSplitShapeFocus, self.woodProbeNode) -- Use the point the player is looking at as a reference system
+end
+
+function ChainsawTest:beforeChainsawDelete(chainswa)
+    unlink(self.woodProbeNode)
+    print("beforeChainsawDelete")
+end
+
+---Gets the radius of the tree at the given location
+---@param shapeId any @The tree shape which was already found
+---@param worldCoorpsNearShape table @World coordinates close to or within the tree
+---@param shapeUnitVectors table @Unit vectors for X/Y/Z dimensions, where the X vector points along the longest dimension of the tree
+---@return any @The ID of the tree shape or nil if it wasn't found at the given location
+---@return number @The radius at the given location
+---@return table @The coordinates a the given location, centered on the tree shape in Y/Z direction
+function ChainsawTest:getRadiusAtLocation(shapeId, worldCoorpsNearShape, shapeUnitVectors)
+
+    -- Define a reasonably large enough rectangle to find the tree
+    -- We already found a tree at this point so we don't need an overly large radius
+    local rectangleSize = 1.1
+    local halfRectangleSize = rectangleSize / 2.0
+
+    -- Move the coordinates half a height in Y and Z direction so that the resulting rectangle will have the original y/z in its center
+    local x = worldCoorpsNearShape.x - shapeUnitVectors.yx*halfRectangleSize - shapeUnitVectors.zx*halfRectangleSize
+    local y = worldCoorpsNearShape.y - shapeUnitVectors.yy*halfRectangleSize - shapeUnitVectors.zy*halfRectangleSize
+    local z = worldCoorpsNearShape.z - shapeUnitVectors.yz*halfRectangleSize - shapeUnitVectors.zz*halfRectangleSize
+
+    -- Find the split shape at the location we already found it at, but this time with a perpendicular cut
+    local minY, maxY, minZ, maxZ = testSplitShape(
+        shapeId,
+        x,y,z,
+        shapeUnitVectors.xx,
+        shapeUnitVectors.xy,
+        shapeUnitVectors.xz,
+        shapeUnitVectors.yx,
+        shapeUnitVectors.yy,
+        shapeUnitVectors.yz,
+        rectangleSize,
+        rectangleSize)
+    if minY ~= nil then
+        local radius = ((maxY-minY)+(maxZ-minZ)) / 4.0 -- /2 for average diameter and another /2 to get the radius
+
+        return shapeId, radius, worldCoorpsNearShape
+    else
+        return nil, 0, {}
+    end
 end
 
 ---Retrieves information about a wood shape at the focus point of the user
 ---@param chainsaw table @The base game chainsaw object
 ---@return any @The ID of the tree shape, or nil if it wasn't found
----@return number @The world X coordinate of the center of the "ring" the player can see
----@return number @The world Y coordinate of the center of the "ring" the player can see
----@return number @The world Z coordinate of the center of the "ring" the player can see
+---@return table @The world X/Y/Z coordinate of the center of the "ring" the player can see
 ---@return number @The radius of the tree shape at the given coordinates
+---@return table @Unit vectors for the tree in X/Y/Z direction, where the tree is aligned around the X axis
 function ChainsawTest:getWoodShapeDimensionsAtFocusPoint(chainsaw)
 
     -- Retrieve the following information from the focus point of the player:
@@ -38,8 +85,7 @@ function ChainsawTest:getWoodShapeDimensionsAtFocusPoint(chainsaw)
     local largeEnoughRectangleSize = 2.0
     local shapeId, minY, maxY, minZ, maxZ = findSplitShape(x,y,z, nx,ny,nz, yx,yy,yz, largeEnoughRectangleSize, largeEnoughRectangleSize)
 
-    local halfRectangleSize = largeEnoughRectangleSize / 2.0
-    local treeCutXWorld, treeCutYWorld, treeCutZWorld, radius
+    local treeCutXWorld, treeCutYWorld, treeCutZWorld, treeCoords, radius, treeUnitVectors
     if shapeId ~= nil and shapeId ~= 0 then
 
         -- Get the radius of the wood shape at the same position, but while ignoring the chainsaw cutting angle
@@ -51,6 +97,11 @@ function ChainsawTest:getWoodShapeDimensionsAtFocusPoint(chainsaw)
 
         -- Get the world coordinates for that point
         treeCutXWorld, treeCutYWorld, treeCutZWorld = localToWorld(self.woodProbeNode, 0,0,0)
+        treeCoords = {
+            x = treeCutXWorld,
+            y = treeCutYWorld,
+            z = treeCutZWorld
+        }
 
         -- Retrieve unit vectors for the local tree coordinate system, but rotate it so that the tree's Y becomes the lookup X axis,
         -- otherwise `testSplitShape` will fail to find anything later on.
@@ -60,21 +111,23 @@ function ChainsawTest:getWoodShapeDimensionsAtFocusPoint(chainsaw)
         yx,yy,yz = localDirectionToWorld(shapeId, 1,0,0) -- unit vector along the local Y axis of the shape, but in world coordinates
         local zx,zy,zz = localDirectionToWorld(shapeId, 0,0,-1) -- unit vector along the local Y axis of the shape, but in world coordinates
 
-        -- Move the coordinates half a height in Y and Z direction so that the resulting rectangle will have the original y/z in its center
-        x = treeCutXWorld - yx*halfRectangleSize - zx*halfRectangleSize
-        y = treeCutYWorld - yy*halfRectangleSize - zy*halfRectangleSize
-        z = treeCutZWorld - yz*halfRectangleSize - zz*halfRectangleSize
+        -- Put unit vectors in a table so other functions can use it afterwards
+        treeUnitVectors = {
+            xx = nx,
+            xy = ny,
+            xz = nz,
+            yx = yx,
+            yy = yy,
+            yz = yz,
+            zx = zx,
+            zy = zy,
+            zz = zz
+        }
 
-        -- Find the split shape at the location we already found it at, but this time with a perpendicular cut
-        local minY2, maxY2, minZ2, maxZ2 = testSplitShape(shapeId, x,y,z, nx,ny,nz, yx,yy,yz, largeEnoughRectangleSize, largeEnoughRectangleSize)
-        if minY2 ~= nil then
-            radius = ((maxY2-minY2)+(maxZ2-minZ2)) / 4.0 -- /2 for average diameter and another /2 to get the radius
-        else
-            shapeId = nil
-        end
+        shapeId, radius, treeCoords = self:getRadiusAtLocation(shapeId, treeCoords, treeUnitVectors)
     end
 
-    return shapeId, treeCutXWorld, treeCutYWorld, treeCutZWorld, radius
+    return shapeId, treeCoords, radius, treeUnitVectors
 end
 
 function ChainsawTest:afterChainsawUpdate(chainsaw)
@@ -83,9 +136,27 @@ function ChainsawTest:afterChainsawUpdate(chainsaw)
     if chainsaw.ringSelector ~= nil and getVisibility(chainsaw.ringSelector) then
 
         -- Find the wood shape we're looking at
-        local shapeId, treeX, treeY, treeZ, radius = self:getWoodShapeDimensionsAtFocusPoint(chainsaw)
+        local shapeId, treeCoords, radius, unitVectors = self:getWoodShapeDimensionsAtFocusPoint(chainsaw)
 
-        Utils.renderTextAtWorldPosition(treeX,treeY,treeZ, ('ShapeId: %d, Radius: %.3f'):format(shapeId, radius), getCorrectTextSize(0.02), 0)
+        if shapeId ~= nil then
+
+            for xOffset = 0, 10 do
+                -- Probe the radius to the left 
+                local x = treeCoords.x + unitVectors.xx * xOffset
+                local y = treeCoords.y + unitVectors.xy * xOffset
+                local z = treeCoords.z + unitVectors.xz * xOffset
+
+                shapeId, radius, _ = self:getRadiusAtLocation(shapeId,  { x = x, y = y, z = z }, unitVectors)
+
+                if shapeId ~= nil then
+                    Utils.renderTextAtWorldPosition(x,y,z, ('%d: %.3f'):format(xOffset, radius), getCorrectTextSize(0.02), 0)
+                else
+                    Utils.renderTextAtWorldPosition(x,y,z, "No data", getCorrectTextSize(0.02), 0)
+                end
+
+                --Utils.renderTextAtWorldPosition(treeX,treeY,treeZ, ('ShapeId: %d, Radius: %.3f'):format(shapeId, radius), getCorrectTextSize(0.02), 0)
+            end
+        end
     end
 
     if not chainsaw.wasAlreadyCutting and chainsaw.isCutting then
--- scripts/TreeValueInfo.lua
+++ scripts/TreeValueInfo.lua
@@ -113,6 +113,11 @@ local function inj_postLoad(chainsaw, superFunc, xmlFile)
     superFunc(chainsaw, xmlFile)
     chainsawTest:afterChainsawPostLoad(chainsaw)
 end
+local function inj_delete(chainsaw, superFunc)
+    chainsawTest:beforeChainsawDelete(chainsaw)
+    superFunc(chainsaw)
+end
 --Chainsaw.updateRingSelector = Utils.overwrittenFunction(Chainsaw.updateRingSelector, ChainsawTest.updateRingSelector)
 Chainsaw.update = Utils.overwrittenFunction(Chainsaw.update, inj_update)
 Chainsaw.postLoad = Utils.overwrittenFunction(Chainsaw.postLoad, inj_postLoad)
+Chainsaw.delete = Utils.overwrittenFunction(Chainsaw.delete, inj_delete)
