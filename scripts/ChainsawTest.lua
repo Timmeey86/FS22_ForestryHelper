56104c8d3f329d591aa5794e7adf57286d4f3ddc
Timmeey86 <Timmeey86@hotmail.com>
2024-01-19 10:16:42 +0100

    properly detect shape (with wrong radius)

--- scripts/ChainsawTest.lua
+++ scripts/ChainsawTest.lua
@@ -16,35 +16,92 @@ end]]
 
 function ChainsawTest:update(chainsaw)
 
+
+    -- If the ring around the tree is currently visible with an equipped chain saw
+    if chainsaw.ringSelector ~= nil and getVisibility(chainsaw.ringSelector) then
+        -- Find the wood shape we're looking at
+        local x,y,z, nx,ny,nz, yx,yy,yz = chainsaw:getCutShapeInformation()
+        local zx,zy,zz = localDirectionToWorld(chainsaw.chainsawSplitShapeFocus, 0,0,1)
+        DebugUtil.drawDebugAreaRectangle(
+            x,y,z,
+            x+yx*chainsaw.cutSizeY, y+yy*chainsaw.cutSizeY, z+yz*chainsaw.cutSizeY,
+            x+zx*chainsaw.cutSizeZ, y+zy*chainsaw.cutSizeZ, z+zz*chainsaw.cutSizeZ,
+            false,
+            1,0,1)
+        DebugUtil.drawDebugLine(x,y,z, x+nx/5,y+ny/5,z+nz/5, 1,0,0, nil, false)
+        DebugUtil.drawDebugLine(x,y,z, x+yx/5,y+yy/5,z+yz/5, 0,1,0, nil, false)
+        DebugUtil.drawDebugLine(x,y,z, x+zx/5,y+zy/5,z+zz/5, 0,0,1, nil, false)
+        local shapeId, minY, maxY, minZ, maxZ = findSplitShape(x,y,z, nx,ny,nz, yx,yy,yz, chainsaw.cutSizeY, chainsaw.cutSizeZ)
+        if shapeId ~= nil and shapeId ~= 0 then
+
+            Utils.renderTextAtWorldPosition(x,y,z, ('Found shapeId: %d'):format(shapeId), getCorrectTextSize(0.05), 0)
+
+            -- Get the radius of the wood shape at the same position, but while ignoring the chainsaw cutting angle
+            -- instead, simulate a perfect perpendicular cut to get the actual radius of the tree
+            -- in order to properly find the shape again, we need to define a rectangle around the whole shape.
+            -- However, x,y,z points to the a point along the local Y axis of the shape (because trees always grow along the Y axis in FS22).
+            -- Therefore, we need to move X and Z half a width/height away to make the local Y axis of the shape end up in the center
+            -- of the rectangle we're defining
+            local yOffset = chainsaw.cutSizeY / 2.0 -- from the point of view of a tree or log, we cut along the X and Z axis
+            local zOffset = chainsaw.cutSizeZ / 2.0
+
+            -- Instead of getting the coordinates of the shape, get the coordinates of the ring selector, since that is centered within the shape,
+            x,y,z = localToWorld(chainsaw.ringSelector, 0,0,0)
+            nx,ny,nz = localDirectionToWorld(shapeId, 0,1,0) -- unit vector along the local X axis of the shape, but in world coordinates
+            yx,yy,yz = localDirectionToWorld(shapeId, 1,0,0) -- unit vector along the local Y axis of the shape, but in world coordinates
+            zx,zy,zz = localDirectionToWorld(shapeId, 0,0,-1) -- unit vector along the local Y axis of the shape, but in world coordinates
+
+            -- but move the coordinates half a height in Y and Z direction so that the resulting rectangle will have the original x/z in its center
+            x = x - yx*yOffset - zx*zOffset
+            y = y - yy*yOffset - zy*zOffset
+            z = z - yz*yOffset - zz*zOffset
+
+            DebugUtil.drawDebugAreaRectangle(
+                x,y,z,
+                x+yx*chainsaw.cutSizeY, y+yy*chainsaw.cutSizeY, z+yz*chainsaw.cutSizeY,
+                x+zx*chainsaw.cutSizeZ, y+zy*chainsaw.cutSizeZ, z+zz*chainsaw.cutSizeZ,
+                false,
+                1,0,1)
+
+
+            DebugUtil.drawDebugLine(x,y,z, x+nx/5,y+ny/5,z+nz/5, 1,0,0, nil, false)
+            DebugUtil.drawDebugLine(x,y,z, x+yx/5,y+yy/5,z+yz/5, 0,1,0, nil, false)
+            DebugUtil.drawDebugLine(x,y,z, x+zx/5,y+zy/5,z+zz/5, 0,0,1, nil, false)
+
+            shapeId, minY, maxY, minZ, maxZ = findSplitShape(x,y,z, nx,ny,nz, yx,yy,yz, chainsaw.cutSizeY, chainsaw.cutSizeZ)
+            if shapeId ~= nil and shapeId ~= 0 then
+                local radius = (minY+maxY+minZ+maxZ)/8.0
+                Utils.renderTextAtWorldPosition(x,y,z, ('Found shapeId: %d'):format(shapeId), getCorrectTextSize(0.05), 0)
+
+                local rX, rY, rZ = localToWorld(chainsaw.ringSelector, 0,0,0)
+                Utils.renderTextAtWorldPosition(rX,rY,rZ, ('Radius: %.3f'):format(radius), getCorrectTextSize(0.05), 0)
+                DebugUtil.drawDebugAreaRectangle(
+                    rX - yx*radius - zx*radius,
+                    rY - yy*radius - zy*radius,
+                    rZ - yz*radius - zz*radius,
+                    rX - yx*radius + zx*radius,
+                    rY - yy*radius + zy*radius,
+                    rZ - yz*radius + zz*radius,
+                    rX + yx*radius - zx*radius,
+                    rY + yy*radius - zy*radius,
+                    rZ + yz*radius - zz*radius,
+                    false,
+                    0,0.5,0.5)
+            end
+
+        end
+    end
+
     if not chainsaw.wasAlreadyCutting and chainsaw.isCutting then
         -- The user has just started cutting
         chainsaw.wasAlreadyCutting = true
 
         local originalShape = chainsaw.curSplitShape
         if originalShape ~= nil then
-
-            -- split the shape, but don't remove the original
-            local x,y,z, nx,ny,nz, yx,yy,yz = chainsaw:getCutShapeInformation()
-            print(tostring(originalShape))
-            local clonedShape = clone(originalShape, false, true, true) -- don't group under parent, don't call "onCreate", don't add physics
-            print(tostring(clonedShape))
-            splitShape(clonedShape, x,y,z, nx,ny,nz, yx,yy,yz, chainsaw.cutSizeY, chainsaw.cutSizeZ, "cutSplitShapeCallback", self)
         end
 
     elseif chainsaw.wasAlreadyCutting and not chainsaw.isCutting then
         -- The user has just stopped cutting
         chainsaw.wasAlreadyCutting = false
     end
-end
-
-
-function ChainsawTest:cutSplitShapeCallback(shape, isBelow, isAbove, minY, maxY, minZ, maxZ)
-    -- Add the shape temporarily to make sure all functions which access it work properly (might not be needed, not sure yet)
-	g_currentMission:addKnownSplitShape(shape)
-    print(tostring(shape))
-    local x,y,z = localToWorld(shape, 0,0,0)
-    -- local nx,ny,nz = localDirectionToWorld(shape, 1,0,0)
-    print(('%d: %.3f, %.3f, %.3f'):format(shape, x, y, z))
-    -- Remove the shape again since we no longer need it (not sure if add + remove leaves a different state than when not calling any of the methods)
-    g_currentMission:removeKnownSplitShape(shape)
 end
