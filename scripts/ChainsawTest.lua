e216cb0c1871f26af639950c7600391181737598
Timmeey86 <Timmeey86@hotmail.com>
2024-01-19 11:17:11 +0100

    fix radius calculation and reduce debug clutter

--- scripts/ChainsawTest.lua
+++ scripts/ChainsawTest.lua
@@ -21,20 +21,8 @@ function ChainsawTest:update(chainsaw)
     if chainsaw.ringSelector ~= nil and getVisibility(chainsaw.ringSelector) then
         -- Find the wood shape we're looking at
         local x,y,z, nx,ny,nz, yx,yy,yz = chainsaw:getCutShapeInformation()
-        local zx,zy,zz = localDirectionToWorld(chainsaw.chainsawSplitShapeFocus, 0,0,1)
-        DebugUtil.drawDebugAreaRectangle(
-            x,y,z,
-            x+yx*chainsaw.cutSizeY, y+yy*chainsaw.cutSizeY, z+yz*chainsaw.cutSizeY,
-            x+zx*chainsaw.cutSizeZ, y+zy*chainsaw.cutSizeZ, z+zz*chainsaw.cutSizeZ,
-            false,
-            1,0,1)
-        DebugUtil.drawDebugLine(x,y,z, x+nx/5,y+ny/5,z+nz/5, 1,0,0, nil, false)
-        DebugUtil.drawDebugLine(x,y,z, x+yx/5,y+yy/5,z+yz/5, 0,1,0, nil, false)
-        DebugUtil.drawDebugLine(x,y,z, x+zx/5,y+zy/5,z+zz/5, 0,0,1, nil, false)
         local shapeId, minY, maxY, minZ, maxZ = findSplitShape(x,y,z, nx,ny,nz, yx,yy,yz, chainsaw.cutSizeY, chainsaw.cutSizeZ)
         if shapeId ~= nil and shapeId ~= 0 then
-
-            Utils.renderTextAtWorldPosition(x,y,z, ('Found shapeId: %d'):format(shapeId), getCorrectTextSize(0.05), 0)
 
             -- Get the radius of the wood shape at the same position, but while ignoring the chainsaw cutting angle
             -- instead, simulate a perfect perpendicular cut to get the actual radius of the tree
@@ -49,7 +37,7 @@ function ChainsawTest:update(chainsaw)
             x,y,z = localToWorld(chainsaw.ringSelector, 0,0,0)
             nx,ny,nz = localDirectionToWorld(shapeId, 0,1,0) -- unit vector along the local X axis of the shape, but in world coordinates
             yx,yy,yz = localDirectionToWorld(shapeId, 1,0,0) -- unit vector along the local Y axis of the shape, but in world coordinates
-            zx,zy,zz = localDirectionToWorld(shapeId, 0,0,-1) -- unit vector along the local Y axis of the shape, but in world coordinates
+            local zx,zy,zz = localDirectionToWorld(shapeId, 0,0,-1) -- unit vector along the local Y axis of the shape, but in world coordinates
 
             -- but move the coordinates half a height in Y and Z direction so that the resulting rectangle will have the original x/z in its center
             x = x - yx*yOffset - zx*zOffset
@@ -70,11 +58,12 @@ function ChainsawTest:update(chainsaw)
 
             shapeId, minY, maxY, minZ, maxZ = findSplitShape(x,y,z, nx,ny,nz, yx,yy,yz, chainsaw.cutSizeY, chainsaw.cutSizeZ)
             if shapeId ~= nil and shapeId ~= 0 then
-                local radius = (minY+maxY+minZ+maxZ)/8.0
-                Utils.renderTextAtWorldPosition(x,y,z, ('Found shapeId: %d'):format(shapeId), getCorrectTextSize(0.05), 0)
+                local radius = ((maxY-minY)+(maxZ-minY)) / 4.0 -- /2 for average diameter and another /2 to get the radius
+                Utils.renderTextAtWorldPosition(x,y,z, ('Found shapeId: %d'):format(shapeId), getCorrectTextSize(0.02), 0)
 
                 local rX, rY, rZ = localToWorld(chainsaw.ringSelector, 0,0,0)
-                Utils.renderTextAtWorldPosition(rX,rY,rZ, ('Radius: %.3f'):format(radius), getCorrectTextSize(0.05), 0)
+                Utils.renderTextAtWorldPosition(rX,rY,rZ, ('Radius: %.3f'):format(radius), getCorrectTextSize(0.02), 0)
+                Utils.renderTextAtWorldPosition(rX,rY+0.2,rZ, ('Y: %.3f/%.3f, Z: %.3f/%.3f'):format(minY, maxY, minZ, maxZ), getCorrectTextSize(0.02), 0)
                 DebugUtil.drawDebugAreaRectangle(
                     rX - yx*radius - zx*radius,
                     rY - yy*radius - zy*radius,
